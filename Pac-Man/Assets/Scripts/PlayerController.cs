aeed6a4cd8bbab114b3cb830ab82b5119253cad2
sarahemottram <70783440+sarahemottram@users.noreply.github.com>
2024-03-02 17:15:06 -0500

    Camera-Relative Movement
    Used cinemachine to create a camera that follows pac-man from behind.  Adjusted movement accordingly to camera.

--- Pac-Man/Assets/Scripts/PlayerController.cs
+++ Pac-Man/Assets/Scripts/PlayerController.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float speed;
-
+    [SerializeField]
+    float rotationSpeed;
 
     private CharacterController characterController;
 
@@ -19,8 +21,33 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        CameraRelativeMovement();
+    }  
 
-        characterController.Move(move * Time.deltaTime * speed);
+    private void CameraRelativeMovement()
+    {
+        // Inputs
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Camera direction
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
+
+        forward.y = 0f; // Ensure the vector is horizontal
+        right.y = 0f; // Ensure the vector is horizontal
+
+        // Relative movement
+        Vector3 moveDirection = (forward * vertical + right * horizontal).normalized;
+
+        // Only rotate the player if moving left or right
+        if (horizontal != 0f) // Moving left or right
+        {
+            Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        // Move the player
+        characterController.Move(moveDirection * Time.deltaTime * speed);
     }
 }
