a12a8128157c459cedead7155f9465eb8297a32d
sarahemottram <70783440+sarahemottram@users.noreply.github.com>
2024-03-04 15:45:08 -0500

    Pac-Man turns with Camera
    Pac-man will turn and face direction of travel, also added wall texture

--- Pac-Man/Assets/Scripts/PlayerController.cs
+++ Pac-Man/Assets/Scripts/PlayerController.cs
@@ -8,15 +8,21 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float speed;
+
     [SerializeField]
-    float rotationSpeed;
+    float cameraRotationSpeed;
+
+    [SerializeField]
+    float modelRotationSpeed;
 
     private CharacterController characterController;
+    private Transform modelTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        modelTransform = transform.Find("Pac-Model");
     }
 
     private void Update()
@@ -34,18 +40,36 @@ public class PlayerController : MonoBehaviour
         Vector3 forward = Camera.main.transform.forward;
         Vector3 right = Camera.main.transform.right;
 
-        forward.y = 0f; // Ensure the vector is horizontal
-        right.y = 0f; // Ensure the vector is horizontal
+        forward.y = 0f; //Vector is horizontal
+        right.y = 0f; //Vector is horizontal
 
         // Relative movement
         Vector3 moveDirection = (forward * vertical + right * horizontal).normalized;
 
         // Only rotate the player if moving left or right
-        if (horizontal != 0f) // Moving left or right
+        if (horizontal != 0f) //left or right
         {
             Quaternion newRotation = Quaternion.LookRotation(moveDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, cameraRotationSpeed * Time.deltaTime);
         }
+
+        //Pac-Man faces direction of travel
+        Quaternion targetRotation;
+
+        if (moveDirection != Vector3.zero) // Moving Forwards
+        {
+            targetRotation = Quaternion.LookRotation(moveDirection);
+        }
+        else if (vertical < 0f) // Moving backward
+        {
+            targetRotation = Quaternion.LookRotation(-forward);
+        }
+        else // No movement
+        {
+            targetRotation = modelTransform.rotation;
+        }
+
+        modelTransform.rotation = Quaternion.Slerp(modelTransform.rotation, targetRotation, modelRotationSpeed * Time.deltaTime);
 
         // Move the player
         characterController.Move(moveDirection * Time.deltaTime * speed);
